19429d7bc2e23d40dfb75ec078f462054cb9c9cd
xiaohu <zhouyi@yx.com>
2024-01-25 11:25:38 +0800

    feat: 数据库连接

--- apps/nest/config/env.ts
+++ apps/nest/config/env.ts
@@ -2,18 +2,17 @@
  * @Author: xiaohu
  * @Date: 2024-01-25 10:31:24
  * @LastEditors: xiaohu
- * @LastEditTime: 2024-01-25 10:45:16
+ * @LastEditTime: 2024-01-25 11:15:47
  * @FilePath: \monorepo-practice\apps\nest\config\env.ts
  * @Description:
  */
-// import fs from 'fs';
-import path from 'path';
-
+import * as path from 'path';
 function parseEnv() {
   const localEnv = path.resolve('.env');
   return {
     path: localEnv,
   };
 }
+const envConfig = parseEnv();
 
-export default parseEnv;
+export default envConfig;
--- apps/nest/src/app.module.ts
+++ apps/nest/src/app.module.ts
@@ -1,18 +1,43 @@
 /*
  * @Author: yeyu98
  * @Date: 2024-01-03 22:08:39
- * @LastEditors: yeyu98
- * @LastEditTime: 2024-01-24 21:02:45
+ * @LastEditors: xiaohu
+ * @LastEditTime: 2024-01-25 11:18:05
  * @Description:
  */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import envConfig from 'config/env';
 
 // 应用程序的根模块，根模块提供了用来启动应用的引导机制，可以包含很多功能模块。
 @Module({
-  imports: [PostsModule],
+  imports: [
+    // 数据库连接配置这里目前不太理解为什么这么做
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [envConfig.path],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // 数据库类型
+        entities: [], // 数据表实体
+        host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT', 3307), // 端口号
+        username: configService.get('DB_USER', 'root'), // 用户名
+        password: configService.get('DB_PASSWORD', '123456'), // 密码
+        database: configService.get('DB_DATABASE', 'blog'), //数据库名
+        timezone: '+08:00', //服务器上配置的时区
+        synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+      }),
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
