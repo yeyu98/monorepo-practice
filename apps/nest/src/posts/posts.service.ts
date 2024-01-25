f6a5b5a9a77fbd6b493c3b6c31370a0d03953f5d
xiaohu <zhouyi@yx.com>
2024-01-25 18:01:21 +0800

    feat: 查询

--- apps/nest/src/posts/posts.service.ts
+++ apps/nest/src/posts/posts.service.ts
@@ -16,6 +16,7 @@ export interface PostsRo {
 // HttpException抛出异常和Error有什么区别
 // PostsRo的作用是什么？
 // InjectRepository是个参数装饰器，具体的作用是什么
+// typeorm 提供了两种查询方式 一种是通过api查询另外一个是通过createQueryBuilder创建查询
 @Injectable()
 export class PostsService {
   constructor(
