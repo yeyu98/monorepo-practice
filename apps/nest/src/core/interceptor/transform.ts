cf5c57a7c5fa46cd03877e1b895746332896d013
yeyu98 <763626908@qq.com>
2024-01-26 21:13:39 +0800

    feat: 拦截器

---
+++ apps/nest/src/core/interceptor/transform.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: yeyu98
+ * @Date: 2024-01-26 21:00:55
+ * @LastEditors: yeyu98
+ * @LastEditTime: 2024-01-26 21:13:20
+ * @Description:
+ */
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+// 这里的map怎么使用？map：可以修改输入的值，再输出。类似于JS的map函数；
+// 这里的Observable 又是个啥？异步流集合（不是很懂
+// 这里的next.handle().pipe 又分别是啥？？
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        code: 0,
+        message: '请求成功',
+      })),
+    );
+  }
+}
--- apps/nest/src/main.ts
+++ apps/nest/src/main.ts
@@ -2,12 +2,13 @@
  * @Author: yeyu98
  * @Date: 2024-01-03 22:08:39
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-01-26 20:50:25
+ * @LastEditTime: 2024-01-26 21:06:38
  * @Description:
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform';
 
 const PORT = 3333;
 async function bootstrap() {
@@ -17,6 +18,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   // 加载过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
+  // 加载拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(PORT);
   console.log(`服务启动咯~ 服务地址是：http://localhost:${PORT}`);
 }
