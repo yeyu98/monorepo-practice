88247523d10347fad0ad87aad18f37a132b1b871
yeyu98 <763525908@qq.com>
2024-04-30 14:34:02 +0800

    feat: 选项

--- apps/react-demo-ts/src/pages/ConvertOptions/ConvertOptions.tsx
+++ apps/react-demo-ts/src/pages/ConvertOptions/ConvertOptions.tsx
@@ -2,19 +2,45 @@
  * @Author: xiaohu
  * @Date: 2024-03-06 17:14:53
  * @LastEditors: xiaohu
- * @LastEditTime: 2024-03-06 17:15:01
+ * @LastEditTime: 2024-03-06 17:30:36
  * @FilePath: \monorepo-practice\apps\react-demo-ts\src\pages\ConvertOptions\ConvertOptions.tsx
  * @Description: 
  */
-import React from 'react'
-import styles from './ConvertOptions.less'
-interface Props {}
+import React, { useState } from 'react'
+import { Input, Typography, Divider } from 'antd'
 
+import styles from './ConvertOptions.less'
+
+const { Title } = Typography
+const { TextArea } = Input
+
+interface Props {}
 function ConvertOptions(props: Props) {
   const {} = props
+  const [name, setName] = useState('')
+  const [config, setConfig] = useState('')
+  const handleNameChange = (event: any) => {
+    const value = event.value
+    setName(value)
+  }
+
+  const handleConfigChange = (event: any) => {
+    const value = event.value
+    setConfig(value)
+  }
 
   return (
-    <></>
+    <>
+      <div className='flex flex-col items-start'>
+   
+        <Title level={5}>标题</Title>
+        <Input placeholder='请输入名称' onChange={handleNameChange} />
+
+        <Title level={5}>选项配置</Title>
+        <TextArea placeholder='请输入选项配置' onChange={handleConfigChange} />
+
+      </div>
+    </>
   )
 }
 
