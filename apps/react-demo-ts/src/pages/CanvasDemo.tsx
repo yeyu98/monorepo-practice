1437f68c6bcdf74c1b050e4975a6e06adb111507
yeyu98 <763525908@qq.com>
2024-06-24 15:30:28 +0800

    feat: 水印

--- apps/react-demo-ts/src/pages/CanvasDemo.tsx
+++ apps/react-demo-ts/src/pages/CanvasDemo.tsx
@@ -2,7 +2,7 @@
  * @Author: yeyu98
  * @Date: 2024-04-30 14:38:15
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-04-30 15:02:24
+ * @LastEditTime: 2024-06-24 15:30:04
  * @FilePath: \monorepo-practice\apps\react-demo-ts\src\pages\CanvasDemo.tsx
  * @Description: 
  */
@@ -17,25 +17,56 @@ interface Props {}
 场景转视图距离计算 dist * zoom;
 视图转场景距离计算 dist / zoom;
 */
+// 水印
+/*
+  绘制水印最终生成图片插入到指定元素背景中
+
+*/ 
 
 function CanvasDemo(props: Props) {
   const {} = props
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const watermarkRef = useRef<HTMLDivElement | null>(null)
+
+  const watermark = ({
+    content = 'hello',
+    width = '200px',
+    height = '150px',
+    textAlign = 'center',
+    textBaseline = 'middle',
+    rotate = 30,
+  }: any) => {
+    const canvas = document.createElement('canvas')
+    canvas.setAttribute('width', width)
+    canvas.setAttribute('height', height)
+    const ctx = canvas.getContext('2d')!
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    ctx.fillRect(0, 0, 800, 450)
+    ctx.textAlign = textAlign
+    ctx.textBaseline = textBaseline
+    ctx.font = '20px microsoft yahei'
+    ctx.rotate(Math.PI / 180 * rotate)
+    ctx.fillText(content, parseFloat(width) / 2, parseFloat(height) / 2);
+    const base64 = canvas.toDataURL('image/png')
+    return base64
+  }
 
   useEffect(() => {
-    if(canvasRef.current) { 
-      const ctx = canvasRef.current.getContext('2d')!
-      ctx.strokeRect(0, 0, 800, 450)
-      ctx.beginPath()
-      ctx.fillStyle = 'blue'
-      ctx.fillRect(350,175, 100, 100)
-      ctx.closePath()
-    }
+    const base64 = watermark({})
+    watermarkRef.current?.setAttribute('style', `
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-repeat: repeat;
+      background-image: url('${base64}');
+    `)
   }, [])
 
   return (
     <>
-      <canvas ref={canvasRef} width={800} height={450}/>
+      {/* <canvas ref={canvasRef} width={800} height={450}/> */}
+      <div ref={watermarkRef}></div>
     </>
   )
 }
