fd63b5900184bdddb6e3438a5a2a04a86ed97b27
yeyu98 <763525908@qq.com>
2024-09-03 09:15:11 +0800

    feat: 路由懒加载

--- apps/react-demo-ts/src/router/index.tsx
+++ apps/react-demo-ts/src/router/index.tsx
@@ -2,25 +2,15 @@
  * @Author: Jerry
  * @Date: 2023-03-03 10:32:42
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-09-02 17:50:24
+ * @LastEditTime: 2024-09-03 09:13:01
  * @FilePath: \monorepo-practice\apps\react-demo-ts\src\router\index.tsx
  * @Description: 
  */
 import { lazy, Suspense } from "react"
 import {Spin} from 'antd'
 import { createBrowserRouter } from "react-router-dom"
-// import App from "@/App"
-// import Demos from "@/pages/Demos"
-// import DanceText from "@/pages/DanceText"
-// import Series from "@/pages/Series"
-// import NestTest from "@/pages/NestTest"
-// import Magnifier from "@/pages/Magnifier/Magnifier"
-// import ConvertVideo from "@/pages/ConvertVideo/ConvertVideo"
-// import ConvertOptions from "@/pages/ConvertOptions/ConvertOptions"
-// import Ocr from "@/pages/Ocr"
-// import CanvasDemo from "@/pages/CanvasDemo"
 
-// FIXME 路由懒加载还有些问题，需要使用Suspense包裹
+
 const App = lazy(() => import('@/App'))
 const Demos = lazy(() => import('@/pages/Demos'))
 const DanceText = lazy(() => import('@/pages/DanceText'))
